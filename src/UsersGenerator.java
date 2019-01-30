2be0a3f899380bdb527f047c51f19f148b58de72
instaHipsta282 <instaHipsta282@users.noreply.github.com>
2019-01-31 01:00:17 +0300

    Commit2

--- src/UsersGenerator.java
+++ src/UsersGenerator.java
@@ -7,15 +7,21 @@ public class UsersGenerator {
 
     public static void main(String[] args) {
         BufferedWriter writer = null;
+        BufferedWriter writer1 = null;
         String formatUsername = "jojo%d";
         String formatPassword = "bean%d";
         try {
+            writer1 = new BufferedWriter(new FileWriter("C:\\Users\\Анна\\Desktop\\Web Tours 1.0\\WebTours\\cgi-bin\\users\\mainFile.dat"));
+            writer1.write("username password\r\n");
+            writer1.flush();
             for (int i = 0; i < 100; i++) {
                 String username = String.format(formatUsername, i);
                 String password = String.format(formatPassword, i);
                 writer = new BufferedWriter(new FileWriter(String.format(userPath, username)));
-                writer.write(password + "\r\n" + username);
+                writer.write(password + "\r\n" + username + ";");
                 writer.flush();
+                writer1.write(username + " " + password + "\r\n");
+                writer1.flush();
             }
         }
         catch (IOException e) { e.printStackTrace(); }
@@ -23,6 +29,7 @@ public class UsersGenerator {
             if (writer != null) {
                 try {
                     writer.close();
+                    writer1.close();
                 }
                 catch (IOException ignore) {}
             }
