54c0a58eac82cc151ad8dc42c8554177192637d7
instaHipsta282 <instaHipsta282@users.noreply.github.com>
2019-02-04 22:19:13 +0300

    Added non-hardcode filepath

--- src/UsersGenerator.java
+++ src/UsersGenerator.java
@@ -1,23 +1,24 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class UsersGenerator {
-    public static final String userPath = "C:\\Users\\Анна\\Desktop\\Web Tours 1.0\\WebTours\\cgi-bin\\users\\%s";
 
     public static void main(String[] args) {
         BufferedWriter writer = null;
         BufferedWriter writer1 = null;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String formatUsername = "jojo%d";
         String formatPassword = "bean%d";
+        System.out.println("Please enter the path to the users folder");
+
         try {
-            writer1 = new BufferedWriter(new FileWriter("C:\\Users\\Анна\\Desktop\\Web Tours 1.0\\WebTours\\cgi-bin\\users\\mainFile.dat"));
+            String usersPath = reader.readLine();
+            writer1 = new BufferedWriter(new FileWriter(usersPath + "\\parametersFile.dat"));
             writer1.write("username password\r\n");
             writer1.flush();
             for (int i = 0; i < 100; i++) {
                 String username = String.format(formatUsername, i);
                 String password = String.format(formatPassword, i);
-                writer = new BufferedWriter(new FileWriter(String.format(userPath, username)));
+                writer = new BufferedWriter(new FileWriter(usersPath + "\\" + username));
                 writer.write(password + "\r\n" + username + ";");
                 writer.flush();
                 writer1.write(username + " " + password + "\r\n");
@@ -30,6 +31,7 @@ public class UsersGenerator {
                 try {
                     writer.close();
                     writer1.close();
+                    reader.close();
                 }
                 catch (IOException ignore) {}
             }
